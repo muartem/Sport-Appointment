c32871c2fbac66af39bf87cde898037834e850ee
Anton Bazhan <bazhan.anton@ukr.net>
2021-03-20 23:51:40 +0200

    feat: add update and delete to service

--- src/redux/serviceReducer.js
+++ src/redux/serviceReducer.js
@@ -1,4 +1,4 @@
-import { ADD_SERVICE, GET_SERVICES, RESET_SERVICES } from "./types";
+import { ADD_SERVICE, DELETE_SERVICE, GET_SERVICES, RESET_SERVICES, UPDATE_SERVICE } from './types';
 
 const initialState = {
   data: [],
@@ -13,10 +13,17 @@ const handlers = {
     ...state,
     data: [...state.data, payload],
   }),
+  [UPDATE_SERVICE]: (state, { payload }) => ({
+    ...state,
+    data: [...state.data.map(item => item.id === payload.id ? payload : item)],
+  }),
+  [DELETE_SERVICE]: (state, { payload }) => ({
+    ...state,
+    data: [...state.data.filter(item => item.id !== payload)],
+  }),
   [RESET_SERVICES]: (state) => initialState,
   DEFAULT: (state) => state,
 };
-
 export const serviceReducer = (state = initialState, action) => {
   const handle = handlers[action.type] || handlers.DEFAULT;
   return handle(state, action);
